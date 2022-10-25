d90ac1ca07b9adf036d62166052fd67c0b231fa4
Shundrik <102665832+Shundrik@users.noreply.github.com>
2022-10-25 10:11:04 +0300

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
+import { Profile } from './Profile/Profile';
+import user from '../data/user.json';
+
+import { FriendList } from './FriendsList/FriendList';
+import friends from '../data/friends.json';
+
+import { Statistics } from './Statistics/Statistics';
+import data from '../data/data.json';
+
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import transactions from '../data/transactions.json'
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      ></Profile>
+
+      <FriendList friends={friends} />
+
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+
+      <TransactionHistory items={transactions} />
+    </>
   );
 };
